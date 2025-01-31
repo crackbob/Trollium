d4b41a2459a696885e4330c33cbdf5807a7c4ef2
cupid <cupiditysxd@gmail.com>
2025-01-30 20:36:39 -0500

    aura

--- src/module/modules/misc/BedAura.js
+++ src/module/modules/misc/BedAura.js
@@ -4,46 +4,78 @@ import hooks from "../../../hooks";
 export default class BedAura extends Module {
     constructor () {
         super("BedAura", "Break beds automatically", "Misc")
+        this.blocks = null;
     }
 
     onGameTick () {
-        let blocks = Object.values(Object.values(hooks.findModule("Gun:class")).find(prop => typeof prop == "object"));
+        if (!this.blocks) {
+            this.blocks = Object.values(Object.values(hooks.findModule("Gun:class"))
+                .find(prop => typeof prop == "object"));
+        }
+
         let heldItem = hooks.noa.ents.getHeldItem(hooks.noa.playerEntity);
         let playerPos = hooks.noa.ents.getPositionData(hooks.noa.playerEntity).position;
         let blockPos = playerPos.map(Math.floor);
-        let blockFound = false;
 
-        let spoofedTargetBlock = {
+        const spoofedTargetBlock = {
             position: {},
-            id: 0,
+            adjacent: {},
+            normal: {},
+            blockID: 0,
             spoofed: true
         }
 
-        for (let x = -5; x <= 5; x++) {
-            for (let y = -5; y <= 5; y++) {
-                for (let z = -5; z <= 5; z++) {
-                    let currentBlock = hooks.noa.bloxd.getBlock(blockPos[0] + x, blockPos[1] + y, blockPos[2] + z);
-                    if (blocks[currentBlock].name.toLowerCase().includes("bed")) {
-                        spoofedTargetBlock.position = [blockPos[0] + x, blockPos[1] + y, blockPos[2] + z];
-                        spoofedTargetBlock.id = blocks[currentBlock].id;
-                        blockFound = true;
-                        break;
-                    }
-                }
-                if (blockFound) break;
-            }
-            if (blockFound) break;
-        }
+        const maxRadius = 5;
+        let found = this.search(blockPos, maxRadius, spoofedTargetBlock);
 
-        if (blockFound) {
+        if (found) {
             hooks.noa.__defineGetter__("targetedBlock", () => spoofedTargetBlock);
             hooks.noa.__defineSetter__("targetedBlock", () => spoofedTargetBlock);
-            heldItem.breaking = true
+            heldItem.breaking = true;
         } else {
             if (hooks.noa?.targetedBlock?.spoofed) {
                 delete hooks.noa.targetedBlock;
                 heldItem.breaking = false;
             }
         }
+    }
+
+    search(blockPos, maxRadius, spoofedTargetBlock) {
+        for (let r = 1; r <= maxRadius; r++) {
+            for (let y = -r; y <= r; y++) {
+                for (let x = -r; x <= r; x++) {
+                    for (let z = -r; z <= r; z++) {
+                        if (Math.max(Math.abs(x), Math.abs(y), Math.abs(z)) != r) continue;
+
+                        let currentBlock = hooks.noa.bloxd.getBlock(
+                            blockPos[0] + x,
+                            blockPos[1] + y,
+                            blockPos[2] + z
+                        );
+                        
+                        if (this.blocks[currentBlock]?.name.toLowerCase().includes("bed")) {
+                            spoofedTargetBlock.position = [
+                                blockPos[0] + x,
+                                blockPos[1] + y,
+                                blockPos[2] + z
+                            ];
+                            spoofedTargetBlock.adjacent = [
+                                blockPos[0] + x,
+                                blockPos[1] + y + 1,
+                                blockPos[2] + z
+                            ];
+                            spoofedTargetBlock.normal = [
+                                0,
+                                0,
+                                0
+                            ]
+                            spoofedTargetBlock.blockID = this.blocks[currentBlock].id;
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
     }
 };
